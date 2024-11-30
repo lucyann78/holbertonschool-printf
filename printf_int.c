c3582e9e3abff2969605453e62f4a4c3cc9ac8e6
lucyann <lucyann78@gmail.com>
2024-11-30 10:39:42 -0400

    print integer

--- printf_int.c
+++ printf_int.c
@@ -1,34 +1,38 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdarg.h>
 /**
- * print_int - prints an integer.
- * @n: the integer to print.
- * Return: void.
+ * printf_integer - prints a signed integer to a buffer.
+ * @b: pointer to the buffer structure.
+ * @val: variadic list containing the signed integer.
+ * Return: signed integer.
  */
-void print_int(int n)
+void printf_integer(buffer *b, va_list val)
 {
+	int n = va_arg(val, int);
+
 	if (n < 0)
 	{
-		_putchar('-');
+		write_buffer(b, '-');
 		n = -n;
 	}
-	if (n == 0)
+	else if (n == 0)
 	{
-		_putchar('0');
+		write_buffer(b, '0');
 		return;
 	}
-	char buffer[10];
+	char temp[11];
 	int i = 0;
 
 	while (n > 0)
 	{
-		buffer[i++] = (n % 10) + '0';
+		temp[i++] = (n % 10) + '0';
 		n /= 10;
 	}
 	int j;
 
 	for (j = i - 1; j >= 0; j--)
 	{
-		_putchar(buffer[j]);
+		write_buffer(b, temp[j];
 	}
 }
